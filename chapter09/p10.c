47e5d33ea400068e03f6f2908cbafa6d89599915
zhaoyining <zyn0212@126.com>
2018-01-24 18:39:18 +0800

    revise chapter09-10

--- chapter09/p10.c
+++ chapter09/p10.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 static int palinkrome(char const *string);
 int main(int argc, char *argv[])
 {
@@ -12,8 +13,13 @@ static int palinkrome(char const *string)
 {
 	char const *head = string;
 	char const *tail = string + strlen(string) - 1;
-	while( head < tail )
-		if( *head++ != *tail-- )
+	while( head < tail ) {
+		if( tolower(*head) != tolower(*tail) )
 			return 0;
+		while( !isalnum(*++head) )
+			continue;
+		while( !isalnum(*--tail) )
+			continue;
+	}
 	return 1;
 }
